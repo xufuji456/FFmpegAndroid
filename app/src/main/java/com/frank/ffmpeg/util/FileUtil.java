65f4ab694fce9db4896990458ebed1efda60b5d4
xufulong <839789740@qq.com>
2019-12-28 14:25:18 +0800

    增加postproc模块的头文件
    增加postproc模块的头文件

--- app/src/main/java/com/frank/ffmpeg/util/FileUtil.java
+++ app/src/main/java/com/frank/ffmpeg/util/FileUtil.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -68,9 +67,7 @@ public class FileUtil {
                 outputStream.write(data, 0, len);
             }
             outputStream.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }catch (IOException e){
+        } catch (IOException e){
             e.printStackTrace();
         }finally {
             try {
