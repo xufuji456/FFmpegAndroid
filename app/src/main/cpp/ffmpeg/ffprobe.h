16e5f65ea19d59d148ac4d08118139428f069a72
xufulong <839789740@qq.com>
2020-01-07 00:22:09 +0800

    ffprobe module: use to probe video or audio format
    ffprobe module: use to probe video or audio format

---
+++ app/src/main/cpp/ffmpeg/ffprobe.h
@@ -0,0 +1,12 @@
+//
+// Created by frank on 2020-01-06.
+//
+
+#ifndef FFMPEGANDROID_FFPROBE_H
+#define FFMPEGANDROID_FFPROBE_H
+
+char* ffprobe_run(int argc, char **argv);
+
+void frank_printf_json(char *fmt, ...);
+
+#endif //FFMPEGANDROID_FFPROBE_H
