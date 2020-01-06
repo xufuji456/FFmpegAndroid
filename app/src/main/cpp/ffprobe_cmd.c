58a8c5c8afff4eb1087e795bb8368407e845cfd7
xufulong <839789740@qq.com>
2020-01-07 00:43:55 +0800

    ffprobe jni interface
    ffprobe jni interface

---
+++ app/src/main/cpp/ffprobe_cmd.c
@@ -0,0 +1,33 @@
+//
+// Created by frank on 2020-01-06.
+//
+
+#include <jni.h>
+#include <string.h>
+#include <malloc.h>
+#include <ffmpeg_jni_define.h>
+#include <ffmpeg/ffprobe.h>
+
+FFMPEG_FUNC(jstring , handleProbe, jobjectArray commands) {
+    int argc = (*env)->GetArrayLength(env, commands);
+    char **argv = (char**)malloc(argc * sizeof(char*));
+    int i;
+    for (i = 0; i < argc; i++) {
+    jstring jstr = (jstring) (*env)->GetObjectArrayElement(env, commands, i);
+    char* temp = (char*) (*env)->GetStringUTFChars(env, jstr, 0);
+    argv[i] = malloc(1024);
+    strcpy(argv[i], temp);
+    (*env)->ReleaseStringUTFChars(env, jstr, temp);
+    }
+    //execute ffprobe command
+    char* result =  ffprobe_run(argc, argv);
+    //release memory
+    for (i = 0; i < argc; i++) {
+    free(argv[i]);
+    }
+    free(argv);
+
+    return (*env)->NewStringUTF(env, result);
+}
+
+
