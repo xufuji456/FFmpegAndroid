d63fb6ae9b80193c7832738e18069f9ffae1918e
xufulong <839789740@qq.com>
2020-04-06 21:46:10 +0800

    translate Pusher into English
    translate Pusher into English

--- app/src/main/java/com/frank/ffmpeg/Pusher.java
+++ app/src/main/java/com/frank/ffmpeg/Pusher.java
@@ -1,7 +1,7 @@
 package com.frank.ffmpeg;
 
 /**
- * 使用FFmepg进行推流直播
+ * Using FFmpeg to push FLV stream
  * Created by frank on 2018/2/2.
  */
 
@@ -10,7 +10,13 @@ public class Pusher {
         System.loadLibrary("media-handle");
     }
 
-    //选择本地文件推流到指定平台直播
+    /**
+     * JNI interface: select file and push to rtmp server
+     *
+     * @param filePath liveUrl
+     * @param liveUrl  the url of rtmp server
+     * @return the result of pushing stream
+     */
     public native int pushStream(String filePath, String liveUrl);
 
 }
