b493895fbcc8e1dc00e8ba6a9bc2b6d74eedebff
xufulong <839789740@qq.com>
2021-01-30 14:27:09 +0800

    change HorizontalAdapter to kotlin

---
+++ app/src/main/java/com/frank/ffmpeg/adapter/HorizontalAdapter.kt
@@ -0,0 +1,57 @@
+package com.frank.ffmpeg.adapter
+
+import android.graphics.Color
+import androidx.recyclerview.widget.RecyclerView
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import com.frank.ffmpeg.R
+import com.frank.ffmpeg.listener.OnItemClickListener
+
+/**
+ * the horizontal adapter of RecyclerView
+ * Created by frank on 2018/6/6.
+ */
+
+class HorizontalAdapter(private val itemList: List<String>?) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var onItemClickListener: OnItemClickListener? = null
+    private var lastClickPosition: Int = 0
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return OkViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_select, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val okViewHolder = holder as OkViewHolder
+        okViewHolder.btnSelect.text = itemList!![position]
+        okViewHolder.btnSelect.setTextColor(Color.DKGRAY)
+        if (onItemClickListener != null) {
+            okViewHolder.btnSelect.setOnClickListener {
+                notifyItemChanged(lastClickPosition)
+                Log.i("onBindViewHolder", "lastClickPosition=$lastClickPosition")
+                //select the current color
+                okViewHolder.btnSelect.setTextColor(Color.BLUE)
+                onItemClickListener!!.onItemClick(okViewHolder.adapterPosition)
+                lastClickPosition = okViewHolder.adapterPosition
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList?.size ?: 0
+    }
+
+    private inner class OkViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var btnSelect: Button = itemView.findViewById(R.id.btn_select)
+
+    }
+
+}
