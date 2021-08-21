1fb74254b0edea822da85ef1e87eed19ee79e6f1
xufuji456 <839789740@qq.com>
2021-08-21 22:22:55 +0800

    remove symbol from macro define

--- app/src/main/cpp/ffmpeg_jni_define.h
+++ app/src/main/cpp/ffmpeg_jni_define.h
@@ -7,8 +7,8 @@
 
 #include <android/log.h>
 
-#define LOGI(TAG, FORMAT, ...) __android_log_print(ANDROID_LOG_INFO, TAG, FORMAT, ##__VA_ARGS__);
-#define LOGE(TAG, FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR, TAG, FORMAT, ##__VA_ARGS__);
+#define LOGI(TAG, FORMAT, ...) __android_log_print(ANDROID_LOG_INFO, TAG, FORMAT, ##__VA_ARGS__)
+#define LOGE(TAG, FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR, TAG, FORMAT, ##__VA_ARGS__)
 
 #define AUDIO_PLAYER_FUNC(RETURN_TYPE, FUNC_NAME, ...) \
 extern "C" { \
