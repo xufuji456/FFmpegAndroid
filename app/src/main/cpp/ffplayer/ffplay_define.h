795fe94d5630bb5b165f342142f66db736d6e509
xufuji456 <xufl@flatincbr.com>
2022-09-23 18:33:25 +0800

    Feature: add PacketQueue and FrameQueue

---
+++ app/src/main/cpp/ffplayer/ffplay_define.h
@@ -0,0 +1,12 @@
+//
+// Created by xu fulong on 2022/9/22.
+//
+
+#ifndef FFMPEGANDROID_FFPLAY_DEFINE_H
+#define FFMPEGANDROID_FFPLAY_DEFINE_H
+
+#define PACKET_QUEUE_SIZE 16
+
+#define FRAME_QUEUE_SIZE 16
+
+#endif //FFMPEGANDROID_FFPLAY_DEFINE_H
