e108a15a8c7d381fce7db10ddd8361909206b01a
xufuji456 <xufl@flatincbr.com>
2022-10-17 13:55:12 +0800

    Feature: remove useless code

--- Live/src/main/res/raw/brightness.glsl
+++
@@ -1,9 +0,0 @@
-varying highp vec2 textureCoordinate;
-
-uniform sampler2D inputImageTexture;
-uniform lowp float brightness;
-
-void main() {
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-    gl_FragColor = vec4((textureColor.rgb + vec3(brightness)), textureColor.w);
-}
