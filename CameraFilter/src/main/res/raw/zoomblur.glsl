a39bd7ffba636bf397484964ee042cc1da8e0914
xufuji456 <xufl@flatincbr.com>
2023-07-01 16:41:52 +0800

    feat: rename param

--- CameraFilter/src/main/res/raw/zoomblur.glsl
+++ CameraFilter/src/main/res/raw/zoomblur.glsl
@@ -7,18 +7,18 @@ uniform highp float blurSize;
 
 void main()
 {
-	// TODO: Do a more intelligent scaling based on resolution here
+
 	highp vec2 samplingOffset = 1.0/100.0 * (blurCenter - textureCoordinate) * blurSize;
 
-	lowp vec4 fragmentColor = texture2D(inputImageTexture, textureCoordinate) * 0.18;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate + samplingOffset) * 0.15;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate + (2.0 * samplingOffset)) *  0.12;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate + (3.0 * samplingOffset)) * 0.09;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate + (4.0 * samplingOffset)) * 0.05;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate - samplingOffset) * 0.15;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate - (2.0 * samplingOffset)) *  0.12;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate - (3.0 * samplingOffset)) * 0.09;
-	fragmentColor += texture2D(inputImageTexture, textureCoordinate - (4.0 * samplingOffset)) * 0.05;
+	lowp vec4 blurColor = texture2D(inputImageTexture, textureCoordinate) * 0.18;
+	blurColor += texture2D(inputImageTexture, textureCoordinate + samplingOffset) * 0.15;
+	blurColor += texture2D(inputImageTexture, textureCoordinate + (2.0 * samplingOffset)) * 0.12;
+	blurColor += texture2D(inputImageTexture, textureCoordinate + (3.0 * samplingOffset)) * 0.09;
+	blurColor += texture2D(inputImageTexture, textureCoordinate + (4.0 * samplingOffset)) * 0.05;
+	blurColor += texture2D(inputImageTexture, textureCoordinate - samplingOffset) * 0.15;
+	blurColor += texture2D(inputImageTexture, textureCoordinate - (2.0 * samplingOffset)) * 0.12;
+	blurColor += texture2D(inputImageTexture, textureCoordinate - (3.0 * samplingOffset)) * 0.09;
+	blurColor += texture2D(inputImageTexture, textureCoordinate - (4.0 * samplingOffset)) * 0.05;
 
-	gl_FragColor = fragmentColor;
+	gl_FragColor = blurColor;
 }
