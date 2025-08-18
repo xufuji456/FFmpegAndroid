345cf528178c7598c50194347e8264386f113e10
xufuji456 <839789740@qq.com>
2025-08-18 18:20:19 +0800

    build x264: support 16kb running

--- shell/x264/build_x264.sh
+++ shell/x264/build_x264.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-archbit=64
+make clean
+
+archbit=$1
 if [ $archbit -eq 32 ];then
 echo "build for 32bit"
-#32bit
+API=21
 ABI='armeabi-v7a'
 CPU='arm'
-ARCH='arm'
 ANDROID='androideabi'
 else
-#64bit
 echo "build for 64bit"
+API=21
 ABI='arm64-v8a'
 CPU='aarch64'
-ARCH='arm64'
 ANDROID='android'
 fi
 
-export NDK=/Users/frank/Downloads/android-ndk-r22b
-export PREBUILT=$NDK/toolchains/$CPU-linux-$ANDROID-4.9/prebuilt
-export PLATFORM=$NDK/platforms/android-21/arch-$ARCH
-export TOOLCHAIN=$PREBUILT/linux-x86_64
-export PREFIX=android/$ABI
+export NDK=/Users/frank/Downloads/android-ndk-r21e
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
+export TARGET=$CPU-linux-$ANDROID
+export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+export AR=$TOOLCHAIN/bin/llvm-ar
+export LD=$TOOLCHAIN/bin/ld
+export PREFIX=$(pwd)/android/$ABI
 
 function build_x264() {
 ./configure \
@@ -29,12 +31,14 @@ function build_x264() {
 --enable-static \
 --disable-asm \
 --enable-pic \
---host=arm-linux \
+--host=$CPU-linux-$ANDROID \
 --cross-prefix=$TOOLCHAIN/bin/$CPU-linux-$ANDROID- \
---sysroot=$PLATFORM \
+--sysroot=$TOOLCHAIN/sysroot \
 --extra-ldflags="-Wl,-z,max-page-size=16384"
 
 make
 make install
 }
 build_x264
+
+echo "build x264 done..."
