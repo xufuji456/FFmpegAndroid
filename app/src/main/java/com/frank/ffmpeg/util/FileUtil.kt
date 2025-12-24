e0fbd6b74aa47cf2aa865249211905f4d1583a5a
xufuji456 <839789740@qq.com>
2025-12-24 20:51:52 +0800

    remove useless file

--- app/src/main/java/com/frank/ffmpeg/util/FileUtil.kt
+++ app/src/main/java/com/frank/ffmpeg/util/FileUtil.kt
@@ -4,7 +4,6 @@ import android.text.TextUtils
 import android.util.Log
 
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 import java.util.Locale.getDefault
@@ -39,55 +38,6 @@ object FileUtil {
     private const val TYPE_3GP = "3gp"
     private const val TYPE_MOV = "mov"
     private const val TYPE_MPG = "mpg"
-
-    fun concatFile(srcFilePath: String, appendFilePath: String, concatFilePath: String): Boolean {
-        if (TextUtils.isEmpty(srcFilePath)
-                || TextUtils.isEmpty(appendFilePath)
-                || TextUtils.isEmpty(concatFilePath)) {
-            return false
-        }
-        val srcFile = File(srcFilePath)
-        if (!srcFile.exists()) {
-            return false
-        }
-        val appendFile = File(appendFilePath)
-        if (!appendFile.exists()) {
-            return false
-        }
-        var outputStream: FileOutputStream? = null
-        var inputStream1: FileInputStream? = null
-        var inputStream2: FileInputStream? = null
-        try {
-            inputStream1 = FileInputStream(srcFile)
-            inputStream2 = FileInputStream(appendFile)
-            outputStream = FileOutputStream(File(concatFilePath))
-            val data = ByteArray(1024)
-            var len = 0
-            while (len >= 0) {
-                len = inputStream1.read(data)
-                if (len > 0) outputStream.write(data, 0, len)
-            }
-            outputStream.flush()
-            len = 0
-            while (len >= 0) {
-                len = inputStream2.read(data)
-                if (len > 0) outputStream.write(data, 0, len)
-            }
-            outputStream.flush()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            try {
-                inputStream1?.close()
-                inputStream2?.close()
-                outputStream?.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
-        }
-        return true
-    }
 
     /**
      * check the file exist or not
@@ -151,18 +101,6 @@ object FileUtil {
         } else fileName.substring(fileName.lastIndexOf("."))
     }
 
-    fun getFilePath(filePath: String): String? {
-        return if (TextUtils.isEmpty(filePath) || !filePath.contains("/")) {
-            null
-        } else filePath.substring(0, filePath.lastIndexOf("/"))
-    }
-
-    fun getFileName(filePath: String): String? {
-        return if (TextUtils.isEmpty(filePath) || !filePath.contains("/")) {
-            null
-        } else filePath.substring(filePath.lastIndexOf("/") + 1)
-    }
-
     fun createListFile(listPath: String, fileList: List<String>?): String? {
         if (TextUtils.isEmpty(listPath) || fileList == null || fileList.isEmpty()) {
             return null
@@ -170,7 +108,7 @@ object FileUtil {
         var outputStream: FileOutputStream? = null
         try {
             val listFile = File(listPath)
-            if (!listFile.parentFile.exists()) {
+            if (listFile.parentFile?.exists() == false) {
                 if (!listFile.mkdirs()) {
                     return null
                 }
@@ -228,7 +166,7 @@ object FileUtil {
             return tempFile.mkdir()
         }
         if (tempFile.isDirectory && tempFile.listFiles() != null) {
-            val files = tempFile.listFiles()
+            val files = tempFile.listFiles() ?: return false
             for (file in files) {
                 result = result and file.delete()
             }
